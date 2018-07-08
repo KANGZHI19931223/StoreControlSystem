a804edde82b532751d59da7094d7204ec92a49e3
KANGZHI19931223 <xk88881@163.com>
2018-07-08 20:55:04 +0800

    完成退出功能

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,18 @@
 <template>
   <el-container class="home">
-    <el-header>Header</el-header>
+    <el-header>
+      <el-row>
+        <el-col :span="4">
+          <img src="/static/images/logo.png" alt="">
+        </el-col>
+        <el-col :span="18">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="2">
+          <a @click.prevent="handleLogout" class="logout" href="#">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside width="200px">Aside</el-aside>
       <el-main>Main</el-main>
@@ -10,7 +22,23 @@
 
 <script>
 export default {
-
+  beforeCreate() {
+    // 验证有没有sessionStorage
+    if (!sessionStorage.getItem('token')) {
+      this.$message.warning('请登录后访问');
+      this.$router.push('login');
+    }
+  },
+  methods: {
+    handleLogout() {
+      // 1 销毁sessionStorage
+      // 2 提示退出成功
+      // 3 跳转到登录页
+      sessionStorage.clear();
+      this.$message.success('退出成功');
+      this.$router.push('login');
+    }
+  }
 };
 </script>
 
@@ -23,6 +51,10 @@ export default {
     color: #333;
     text-align: center;
     line-height: 60px;
+  }
+  .el-header .logout {
+    text-decoration: none;
+    color: orange;
   }
   .el-aside {
     background-color: #D3DCE6;
