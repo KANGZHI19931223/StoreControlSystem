513e5970acbc093690c59fa15a62dc1f55e61a0a
KANGZHI19931223 <xk88881@163.com>
2018-07-12 18:04:33 +0800

    完成商品分类分页和部分页面展示

--- src/views/goods/Categories.vue
+++ src/views/goods/Categories.vue
@@ -6,7 +6,7 @@
      <el-button type="success" plain class="add-categories">添加分类</el-button>
      <!-- 分类列表 -->
      <el-table
-      :data="tableData"
+      :data="goodsList"
       style="width: 100%">
       <el-table-column
         prop="cat_name"
@@ -14,17 +14,23 @@
         width="180">
       </el-table-column>
       <el-table-column
-        prop="cat_level"
         label="级别"
         width="180">
+        <template slot-scope="scope">
+          <span v-if="scope.row.cat_level === 0">一级</span>
+          <span v-else-if="scope.row.cat_level === 1">二级</span>
+          <span v-else-if="scope.row.cat_level === 2">三级</span>
+        </template>
       </el-table-column>
       <el-table-column
-        prop="address"
         label="是否有效">
+        <template slot-scope="scope">
+          {{ scope.row.cat_deleted ? '无效' : '有效' }}
+        </template>
       </el-table-column>
       <el-table-column
         label="操作">
-        <template>
+        <template slot-scope="scope">
           <el-button
             plain size="mini"
             type="primary"
@@ -42,18 +48,52 @@
     <el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
-      :current-page="currentPage4"
-      :page-sizes="[100, 200, 300, 400]"
-      :page-size="100"
+      :current-page="pagenum"
+      :page-sizes="[5, 10, 15, 20]"
+      :page-size="pagesize"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="400">
+      :total="total">
     </el-pagination>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      goodsList: [],
+      total: 0,
+      pagenum: 1,
+      pagesize: 10
+    };
+  },
+  created() {
+    this.getCate();
+  },
+  methods: {
+    // 获取一级菜单
+    async getCate() {
+      const {data: resData} = await this.$http.get(`categories?type=3&pagenum=${this.pagenum}&pagesize=${this.pagesize}`);
+      const {data, meta: {msg, status}} = resData;
+      if (status === 200) {
+        this.goodsList = data.result;
+        this.total = data.total;
+        console.log(data);
+      } else {
+        this.$message.error(msg);
+      }
+    },
+    // 每页显示数据条数改变事件
+    handleSizeChange(val) {
+      this.pagesize = val;
+      this.getCate();
+    },
+    // 当前页数改变事件
+    handleCurrentChange(val) {
+      this.pagenum = val;
+      this.getCate();
+    }
+  }
 };
 </script>
 
